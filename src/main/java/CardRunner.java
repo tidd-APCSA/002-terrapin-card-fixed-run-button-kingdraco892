3c68fd9c849205f260eb761d2e8a4369ff70a46e
Sahil Patel <708111@aacps.org>
2020-10-16 13:52:06 +0000

    worked through most of it

--- src/main/java/CardRunner.java
+++ src/main/java/CardRunner.java
@@ -1,7 +1,16 @@
 public class CardRunner {
     public static void main(String args[]){
-        // ☕
-        System.out.println("hey");
+        TerrapinCard card = new TerrapinCard (10);
+System.out.println(card);
+
+      card.loadMoney(15);
+      System.out.println(card);
+
+      card.loadMoney(10);
+      System.out.println(card);
+
+      card.loadMoney(200);
+      System.out.println(card);
 
     }
 }
--- src/main/java/TerrapinCard.java
+++ src/main/java/TerrapinCard.java
@@ -1,4 +1,27 @@
 public class TerrapinCard {
+private double balance;
+public TerrapinCard(double balanceAtStart){
+  balance = balanceAtStart;
+}
+public void payEconomical(){
+  if(balance >= 2.50){
+  balance -= 2.50;
+  }
+  
+}
+public void payGourmet(){
+  if(balance >= 4.00){
+  balance -= 4.00;
+  }
+}
+  public void loadMoney(double amount){
+    balance = balance + amount;
+    if( balance >= 150){
+      balance = 150.00;
+    }
+  }
 
-
+public String toString(){
+  return "This card has " + balance +" dollars";
+}
 }
